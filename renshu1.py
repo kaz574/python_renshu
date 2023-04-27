d5473da586b679c0c6768643b83cae246632ba70
kazu.14.hide@gmail.com <kazu.14.hide@gmail.com>
2023-04-27 12:26:53 +0900

    30ページまでの練習

--- renshu1.py
+++ renshu1.py
@@ -1,1 +1,24 @@
-#python練習用のエディター
+#for countdown in 1,2,3,4,5, "hey":
+    #print(countdown)
+
+#import this
+#print(8*9)
+#a = 7
+#print(a)
+#print(type(a))
+#print(9/5)
+#print(9//5)
+
+##30ページまでの練習
+
+
+
+
+
+
+
+
+
+
+
+
