ce48885404c963e40c165b3cad7de2d782706049
kazu.14.hide@gmail.com <kazu.14.hide@gmail.com>
2023-05-01 17:05:30 +0900

    90ページまでの変更

--- renshu1.py
+++ renshu1.py
@@ -11,14 +11,18 @@
 
 ##30ページまでの練習
 
+#型の変換
+#print(a)
+#print(int (a))
+#print("He said 'I have a pen'")
+#print("I like 'cory henry' nNa Na Na")
 
+#a = "hello"
+#print(a[1:])
+#print(len(a))
 
+#food = ["tomato", "banana"]
+#food.append("apple")
+#print(food)
 
-
-
-
-
-
-
-
-
+##90ページまでの練習
